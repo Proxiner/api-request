4474dc929a0588442df6587496056d581c885e28
proxiner <mahdigamingcodez@gmail.com>
2024-06-27 20:35:31 +0330

    -a

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,60 +3,58 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Users.css";
 
 function Users() {
+
   const [usersList, setUsersList] = useState([]);
-  const [searched, setSearched] = useState(false);
-  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const [searchedUser , setSearchedUser] = useState([]);
+
   const inputTag = useRef();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((output) => setUsersList(output));
-  }, []);
-
+    }, []);
+    
   const fetchUser = () => {
-    setSearched(true);
-    const searchQuery = inputTag.current.value.toLowerCase();
+    fetch(`https://jsonplaceholder.typicode.com/users?name=${inputTag.current.value}`)
+    .then(response => response.json())
+    .then(output => setSearchedUser(output))
+  }
 
-    if (searchQuery === "") {
-      setSearched(false);
-      setFilteredUsers([]);
-      return;
-    }
-
-    const matchedUsers = usersList.filter((user) =>
-      user.name.toLowerCase().includes(searchQuery)
-    );
-
-    setFilteredUsers(matchedUsers);
-  };
+  const mathcedUsers = usersList.filter((user)=>{
+    user.name.toLowerCase().includes(searchQuery)
+  })
 
   return (
     <div className="container">
       <input
         type="text"
         ref={inputTag}
-        onInput={fetchUser}
         placeholder="Search:"
       />
+      <button onClick={fetchUser}> search </button>
       <div className="row">
-        {!searched
-          ? usersList.map((user) => (
-              <ul key={user.id}>
-                <li>Name: {user.name}</li>
-                <li>Email: {user.email}</li>
-                <li>Phone: {user.phone}</li>
-                <li>Website: {user.website}</li>
-              </ul>
-            ))
-          : filteredUsers.map((user) => (
-              <ul key={user.id}>
-                <li>Name: {user.name}</li>
-                <li>Email: {user.email}</li>
-                <li>Phone: {user.phone}</li>
-                <li>Website: {user.website}</li>
-              </ul>
-            ))}
+        {usersList.map((user) => (
+          <ul key={user.id}>
+            <li> {user.name} </li>
+            <li> {user.email} </li>
+            <li> {user.phone}</li>
+            <li> {user.website} </li>
+          </ul>
+        ))}
+      </div>
+      <div className="row">
+        {searchedUser.map((user)=>(
+           <ul key={user.id}>
+            <li> {user.name} </li>
+            <li> {user.email} </li>
+            <li> {user.phone}</li>
+            <li> {user.website} </li>
+           </ul>
+        ))}
+
+        <p>{JSON.stringify(mathcedUsers)}</p>
       </div>
     </div>
   );
